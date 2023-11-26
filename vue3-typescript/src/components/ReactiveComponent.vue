27a42aad72927398fdb085254decce284554f1d5
takyafumin <takyafumin@gmail.com>
2023-11-26 19:23:20 +0900

    セクション4: Typescript x Vue3

---
+++ vue3-typescript/src/components/ReactiveComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+type User = {
+    name: string
+    age: number
+}
+
+const user1: User = {
+    name: 'Taro',
+    age: 20
+}
+
+const user2 = reactive({} as User)
+user2.name = 'Hoge'
+
+</script>
+
+<template>
+    <h2>Reactive</h2>
+    {{ user1.name }}<br>
+    {{ user2.name }}
+</template>
